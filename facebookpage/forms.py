67db88f3aa3f5ce6c2fd34f4f6e21ee1b1eaa54a
Kalabha P Vinod <kalabha.p.vinod@gmail.com>
2020-01-28 20:53:20 +0530

    fix form validation

--- facebookpage/forms.py
+++ facebookpage/forms.py
@@ -2,7 +2,9 @@ from django import forms
 
 
 class FbForm(forms.Form):
-    about = forms.CharField(widget=forms.Textarea(attrs={'rows':"5", 'class':"form-control form-control-line"}))
-    description = forms.CharField(widget=forms.Textarea(attrs={'rows':"5", 'class':"form-control form-control-line"}))
+    about = forms.CharField(required=False,
+                            widget=forms.Textarea(attrs={'rows': "5", 'class': "form-control form-control-line", }), )
+    description = forms.CharField(required=False,
+                                  widget=forms.Textarea(attrs={'rows': "5", 'class': "form-control form-control-line"}))
     id = forms.CharField()
     access_token = forms.CharField(widget=forms.Textarea)
